db6ba96510bfdf16f6b2f6c16d69ddd09851283d
Farhan <ullah8113@gmail.com>
2025-08-16 20:45:28 +0600

    Check user

---
+++ app/(main)/layout.js
@@ -0,0 +1,5 @@
+const MainLayout = ({ children }) => {
+  return <div container="container mx-auto mt-24 mb-20">{children}</div>;
+};
+
+export default MainLayout;
--- components/Header.jsx
+++ components/Header.jsx
@@ -4,6 +4,7 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { checkUser } from "@/lib/checkUser";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import {
   ChevronDown,
@@ -18,6 +19,7 @@ import Logo from "./Logo";
 import { Button } from "./ui/button";
 
 export default async function Header() {
+  await checkUser();
   return (
     <header className="fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60">
       <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
