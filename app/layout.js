e072aaecd0d1ef89c671d1cc6232243e91e2566f
Farhan <ullah8113@gmail.com>
2025-09-01 14:03:32 +0600

    Fixed

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,9 @@
-import Header from "@/components/header";
 import { ThemeProvider } from "@/components/theme-provider";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import { Inter } from "next/font/google";
 import { Toaster } from "sonner";
+import Header from "../components/header";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
